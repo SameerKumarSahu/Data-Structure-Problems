f4938bf132b0711d786c25db65040fa3ec3373fd
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-26 15:55:02 +0530

    Add to node

--- LinkedLIst.java
+++ LinkedLIst.java
@@ -1,10 +1,51 @@
 package com.SingleLinkedLIst;
 
-public class LinkedLIst {
+public class LinkedList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Linked List Data Structure Program...");
+	class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
 	}
 
+	public Node head = null;
+	public Node tail = null;
+
+	  public void display() {    
+	        //Node current will point to head    
+	        Node current = head;    
+	            
+	        if(head == null) {    
+	            System.out.println("List is empty");    
+	            return;    
+	        }    
+	        System.out.println("Nodes of singly linked list: ");    
+	        while(current != null) {    
+	            //Prints each node by incrementing pointer    
+	            System.out.print(current.data + "-->");    
+	            current = current.next;    
+	        }    
+	        System.out.println();    
+	    }    
+	  public void addToBegin(int x){
+	        if (head == null){
+	            head = new Node(x);
+	        }else{
+	            Node temp = new Node(x);
+	            temp.next = head;
+	            head = temp;
+	        }
+	     }
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.addToBegin(70);
+		list.addToBegin(30);
+		list.addToBegin(56);
+		list.display();    
+	}
+	
 }
