113171c04348f6ac549f03ff4da6095ff9ba8473
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-26 15:36:34 +0530

    Welcome Message

---
+++ LinkedLIst.java
@@ -0,0 +1,10 @@
+package com.SingleLinkedLIst;
+
+public class LinkedLIst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Linked List Data Structure Program...");
+	}
+
+}
