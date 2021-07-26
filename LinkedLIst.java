1f969e951abf933954a97518f6ef03dd0ac986fb
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-26 16:14:11 +0530

    Inserting data

--- LinkedLIst.java
+++ LinkedLIst.java
@@ -23,7 +23,7 @@ public class LinkedList {
 	            System.out.println("List is empty");    
 	            return;    
 	        }    
-	        System.out.println("Nodes of singly linked list: ");    
+	           
 	        while(current != null) {    
 	            //Prints each node by incrementing pointer    
 	            System.out.print(current.data + "-->");    
@@ -31,23 +31,37 @@ public class LinkedList {
 	        }    
 	        System.out.println();    
 	    }    
-	  public void addToEnd(int x){
-	        Node node = new Node(x);
-	        if (head == null){
-	            this.head = node;
-	        }else{
-	            Node temp = head;
-	            while (temp.next != null){
-	                temp = temp.next;
-	            }
-	            temp.next = node;
+	  public void addnode(int data) {    
+	        //Create a new node    
+	        Node newNode = new Node(data); 
+	        if(head == null) {    
+	            //If list is empty, both head and tail will point to new node    
+	            head = newNode;    
+	            tail = newNode;    
+	        }    
+	        else { 
+	            tail.next = newNode;    
+	            //newNode will become new tail of the list    
+	            tail = newNode;    
+	        }    
+	    }   
+	  public void insert(int position,int value){
+	        Node newNode = new Node(value);
+	        Node temp = head;
+	        for (int i=1; i < position-1; i++) {
+	            temp = temp.next;
 	        }
+	        newNode.next = temp.next;
+	        temp.next=newNode;
+
+	        System.out.println(value +" Added sucessfully between 56 and 70");
 	    }
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-		list.addToEnd(56);
-		list.addToEnd(30);
-		list.addToEnd(70);
+		
+		list.addnode(56);
+		list.addnode(70);
+		list.insert(1,30);
 		list.display();    
 	}
 	
